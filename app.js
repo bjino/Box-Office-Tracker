223658c2f76dc1cf73c529a043dafee00fd09165
Byron Inocencio <jay.byron@gmail.com>
2016-08-30 06:54:37 +0000

    configured mysql to webserver

--- app.js
+++ app.js
@@ -2,7 +2,25 @@ var express = require('express');
 var app = express();
 var morgan = require('morgan');
 var path = require('path');
+var fs = require('fs');
+var mysql = require('mysql');
 
+var ccontent= fs.readFileSync(".dbinfo.json");
+var cf = JSON.parse(ccontent);
+
+
+var connection = mysql.createConnection({
+   host   : cf.host,
+   user   : cf.user,
+   password : cf.password,
+   database: cf.database
+});
+
+connection.connect();
+connection.query('SELECT 1 + 1 AS solution', function(err,rows,fields){
+   if (err) throw err;
+   console.log('The solution is: ', rows[0].solution);
+});
 app.use(morgan('dev'));
 
 app.get('/', function(req,res){
@@ -12,5 +30,7 @@ app.get('/', function(req,res){
 app.listen(3000, function(){
 
    console.log('Example app listening port 3000!');
-
+   console.log(cf.host);
 });
+
+connection.end();
